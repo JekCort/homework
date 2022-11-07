22763fd31f5720c3dad3a2211513ccc96f49672c
Yevgen Kostenko <jekcort969@gmail.com>
2022-11-07 23:08:55 +0200

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,22 @@
 import {UserType} from '../HW8'
+import {UserPropsType} from "../User";
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+                    })
+            return action.payload === 'up' ? newState : newState.reverse() // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(a => a.age >= action.payload) // need to fix
         }
         default:
             return state
